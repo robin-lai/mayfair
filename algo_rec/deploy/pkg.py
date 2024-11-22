c58d442e6b50fbbd819854a77e919552b6fc16ad
dehe.lai <dehe.lai@urbanic.com>
2024-11-22 18:06:23 +0800

    debug

--- algo_rec/deploy/pkg.py
+++ algo_rec/deploy/pkg.py
@@ -2,7 +2,8 @@
 import boto3
 import sagemaker
 from sagemaker import get_execution_role
-import sh
+# import sh
+import os
 from constant import *
 
 if __name__ == '__main__':
@@ -11,9 +12,12 @@ if __name__ == '__main__':
     deploy_code_dir = deploy_tmp_dir + 'code/'
     code_file = deploy_dir + 'inference.py'
     # sh.cd(deploy_dir)
-    sh.mkdir(deploy_tmp_dir)
-    sh.mkdir(deploy_code_dir)
-    sh.cp(code_file, deploy_code_dir)
+    os.system('mkdir %s' % deploy_tmp_dir)
+    # sh.mkdir(deploy_tmp_dir)
+    # sh.mkdir(deploy_code_dir)
+    os.system('mkdir %s' % deploy_code_dir)
+    os.system('cp %s %s'%(code_file,deploy_code_dir ))
+    # sh.cp(code_file, deploy_code_dir)
 
     s3_cli = boto3.client('s3')
     sm_sess = sagemaker.Session()
@@ -25,7 +29,6 @@ if __name__ == '__main__':
     # download files
     BUCKET = 'warehouse-algo'
     # s3_cli.download_file(Bucket=BUCKET, Key=s3_model, Filename=deploy_tmp_dir)
-    import os
     os.system('aws s3 cp --recursive %s %s' % (s3_model, deploy_tmp_dir))
     # tar
     tar_file = deploy_tmp_dir + tar_name
