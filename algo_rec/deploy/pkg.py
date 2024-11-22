1ef046f4c368aff6f3e3b66c8f0c3f4ca9b99766
dehe.lai <dehe.lai@urbanic.com>
2024-11-22 18:04:54 +0800

    debug

--- algo_rec/deploy/pkg.py
+++ algo_rec/deploy/pkg.py
@@ -29,7 +29,8 @@ if __name__ == '__main__':
     os.system('aws s3 cp --recursive %s %s' % (s3_model, deploy_tmp_dir))
     # tar
     tar_file = deploy_tmp_dir + tar_name
-    sh.tar("czvf", tar_file, deploy_tmp_dir)
+    os.system('tar -czvf  %s  %s' % (tar_file, deploy_tmp_dir))
+    # sh.tar("czvf", tar_file, deploy_tmp_dir)
     # upload
     s3_model_online_tar_file = s3_model_online + tar_file
     os.system('aws s3 cp %s %s' % (tar_file, s3_model_online_tar_file))
