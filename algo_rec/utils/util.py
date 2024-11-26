8b1f5716f803a8b135f4639028a4eca75d1d44e4
dehe.lai <dehe.lai@urbanic.com>
2024-11-26 19:58:30 +0800

    D

--- algo_rec/utils/util.py
+++ algo_rec/utils/util.py
@@ -3,4 +3,4 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     c = math.ceil(len(lst) / n)
     for i in range(0, len(lst), c):
-        yield lst[i:i + n]
+        yield lst[i:i + c]
