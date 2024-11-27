93f6004057de3283406db0436ee17743d21e7d21
laidehe <laidehe>
2024-11-27 08:15:03 +0000

    tfrecored

--- algo_rec/learn/tf115_eager.py
+++ algo_rec/learn/tf115_eager.py
@@ -3,7 +3,8 @@ tf.compat.v1.enable_eager_execution()
 # dataset = tf.data.TFRecordDataset('./tfrecord/part-00000-827236cb-422b-4758-9f33-265565f1aad3-c000')
 # dataset2 =tf.data.TFRecordDataset('./tfrecord/part-00199-827236cb-422b-4758-9f33-265565f1aad3-c000')
 filenames = ["/home/sagemaker-user/mayfair/algo_rec/rank/exp/cn_rec_detail_sample_v1_tfr-all/ds=20241112/part-00003-827236cb-422b-4758-9f33-265565f1aad3-c000",
-             "/home/sagemaker-user/mayfair/algo_rec/rank/exp/cn_rec_detail_sample_v1_tfr-all/ds=20241112/part-00002-827236cb-422b-4758-9f33-265565f1aad3-c000"]
+             "/home/sagemaker-user/mayfair/algo_rec/rank/exp/cn_rec_detail_sample_v1_tfr-all/ds=20241112/part-00002-827236cb-422b-4758-9f33-265565f1aad3-c000",
+             "/home/sagemaker-user/mayfair/algo_rec/rank/exp/cn_rec_detail_sample_v1_tfr-all/ds=20241112/part-00001-827236cb-422b-4758-9f33-265565f1aad3-c000"]
 dataset3 = tf.data.TFRecordDataset(filenames)
 n1 = 0
 n2 = 0
