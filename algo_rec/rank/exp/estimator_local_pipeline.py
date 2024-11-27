3312b01da06d2f938c5f6e9d6d69ec7df097c445
dehe.lai <dehe.lai@urbanic.com>
2024-11-27 16:28:39 +0800

    predict

--- algo_rec/rank/exp/estimator_local_pipeline.py
+++ algo_rec/rank/exp/estimator_local_pipeline.py
@@ -280,6 +280,14 @@ def main(args):
     # print("predictions:", predictions)
     # print("predictions:", [v['probabilities'] for v in list(predictions)])
 
+    print('begin predict', '#' * 80)
+    pred = estimator.predict(input_fn=eval_input_fn)
+    pred_np = pred.numpy()
+    np.save(args.pred_save_file,pred_np)
+    print('end predict', '#' * 80)
+    print('pred_head 100 element:', pred_np[0:100])
+    print('save pred result to file:')
+
 
 
     def make_serving_input_receiver_fn():
@@ -346,4 +354,6 @@ if __name__ == "__main__":
     tf.app.flags.DEFINE_string("train_path", "/home/sagemaker-user/mayfair/algo_rec/rank/exp/cn_rec_detail_sample_v1_tfr-all/ds=20241112/part-00003-827236cb-422b-4758-9f33-265565f1aad3-c000", "")
     tf.app.flags.DEFINE_string("model_dir",'/home/sagemaker-user/mayfair/algo_rec/rank/exp/model_seq2', "")
     tf.app.flags.DEFINE_string("target", "ctr", "contracted")
+    tf.app.flags.DEFINE_string("pred_save_file", "./pred_ret_np.npy", "")
+
     main(FLAGS)
