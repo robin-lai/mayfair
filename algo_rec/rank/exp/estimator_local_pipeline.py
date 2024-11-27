a3977d74f93bfe244360c4c692cec8abfb32baed
dehe.lai <dehe.lai@urbanic.com>
2024-11-27 17:00:39 +0800

    predict

--- algo_rec/rank/exp/estimator_local_pipeline.py
+++ algo_rec/rank/exp/estimator_local_pipeline.py
@@ -286,7 +286,7 @@ def main(args):
     pred = estimator.predict(input_fn=eval_input_fn)
     pred_list = []
     for ele in pred:
-        pred_list.append(ele)
+        pred_list.append(ele) # ele is dict
     with open(args.pred_save_file, 'wb') as fout:
         pickle.dump(pred_list, fout)
     print('end predict', '#' * 80)
