e66e1fb6def78091d06063874d05f8a0c7d3a69d
laidehe <laidehe>
2024-11-29 03:38:25 +0000

    D

--- algo_rec/learn/tfrecord_write.py
+++ algo_rec/learn/tfrecord_write.py
@@ -1,3 +1,3 @@
 import tensorflow as tf
 tf.compat.v1.enable_eager_execution()
-fout_cvr = tf.io.TFRecordWriter('xxxx')
+fout_cvr = tf.io.TFRecordWriter('/home/sagemaker-user/mayfair/algo_rec/data/cn_rec_detail_sample_v1_ctr/ds=20241102/part-00033-84854856-90d1-4bf8-9e52-5032827fdee4-c000')
