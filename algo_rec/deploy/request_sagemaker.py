82aa43b5071dbd7498d8b30055923f74b5425e61
dehe.lai <dehe.lai@urbanic.com>
2024-11-29 14:28:36 +0800

    fix

--- algo_rec/deploy/request_sagemaker.py
+++ algo_rec/deploy/request_sagemaker.py
@@ -21,13 +21,19 @@ request = {
         "high_level_seq": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
                            "18", "19", "20"],
         "userFeatures": {
-            "high_level_seq": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18","19", "20"],
+            "high_level_seq": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16",
+                               "17", "18", "19", "20"],
             "low_level_seq": []
         },
         "contextFeatures": {},
         "itemFeatures": {}
     }
 }
+
+request2 = {"city": "Menbai", "country": "IN", "debug": "",
+            "featureMap": {"high_level_seq": [], "userFeatures": {"high_level_seq": [], "low_level_seq": []}},
+            "goodsIdList": ["1327692", "1402902"], "ip": "127.0.0.1", "platform": "H5", "province": "Menbai",
+            "scene": "detail_rec", "userId": "23221", "userNo": "2321", "uuid": "fxleyu", "version": "8.2.2"}
 
 sg_client = boto3.client("sagemaker-runtime")
 
@@ -37,7 +43,7 @@ if __name__ == '__main__':
 
     res = sg_client.invoke_endpoint(
         EndpointName=endpoint,
-        Body=json.dumps(request),
+        Body=json.dumps(request2),
         # Body=json.dumps(ipt4).encode('utf-8'),
         # .encode('utf-8'),
         # Body=ipt4,
