231dcb5d3b6db26d7ebd54752ad0195b5a13bb30
dehe.lai <dehe.lai@urbanic.com>
2024-12-03 17:00:56 +0800

    predict

--- algo_rec/rank/prod/predict.py
+++ algo_rec/rank/prod/predict.py
@@ -25,8 +25,33 @@ def get_infer_tensor_dict():
              "19", "20"], dtype=tf.string),
         "cate_id": tf.constant(["1"], dtype=tf.string),
 
+    },
+    tensor_dict2 = {
+        "cate_level1_id": tf.constant([["1"]], dtype=tf.string),
+        "cate_level2_id": tf.constant([["1"]], dtype=tf.string),
+        "cate_level3_id": tf.constant([["1"]], dtype=tf.string),
+        "cate_level4_id": tf.constant([["1"]], dtype=tf.string),
+        "country": tf.constant([["1"]], dtype=tf.string),
+        "ctr_7d": tf.constant([[0.1]], dtype=tf.float32),
+        "cvr_7d": tf.constant([[0.1]], dtype=tf.float32),
+        "show_7d": tf.constant([[100]], dtype=tf.int32),
+        "click_7d": tf.constant([[100]], dtype=tf.int32),
+        "cart_7d": tf.constant([[100]], dtype=tf.int32),
+        "ord_total": tf.constant([[100]], dtype=tf.int32),
+        "pay_total": tf.constant([[100]], dtype=tf.int32),
+        "ord_7d": tf.constant([[100]], dtype=tf.int32),
+        "pay_7d": tf.constant([[100]], dtype=tf.int32),
+        "seq_goods_id": tf.constant(
+            [["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18",
+             "19", "20"]], dtype=tf.string),
+        "goods_id": tf.constant([["1"]], dtype=tf.string),
+        "seq_cate_id": tf.constant(
+            [["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18",
+             "19", "20"]], dtype=tf.string),
+        "cate_id": tf.constant([["1"]], dtype=tf.string),
+
     }
-    return tensor_dict
+    return tensor_dict2
 local_model_dir = '/home/sagemaker-user/mayfair/algo_rec/deploy/tmp/1733194146'
 predictor = tf.saved_model.load_v2(local_model_dir).signatures["serving_default"]
 tensor_dict = get_infer_tensor_dict()
