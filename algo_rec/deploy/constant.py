5274df4b84e8ff363995bfed40f65532b2c1fcf6
dehe.lai <dehe.lai@urbanic.com>
2024-12-04 11:52:34 +0800

    identity

--- algo_rec/deploy/constant.py
+++ algo_rec/deploy/constant.py
@@ -18,13 +18,13 @@ fts_user_seq_off_pickle = deploy_pkg_dir + 'user_seq_off_features.pkl'
 
 # config
 # s3_model = 's3://warehouse-algo/rec/prod_model/prod_ctr_seq_off_din_v0/ds=20241107/model/'
-s3_model = 's3://warehouse-algo/rec/test_model/predict_test3/ds=20241112/model'
+s3_model = 's3://warehouse-algo/rec/test_model/predict_test4/ds=20241112/model'
 model_local = '~/mayfair/algo_rec/rank/exp/model_seq_nohead_1day_1128/1732783231/'
 # s3_model_online = 's3://algo-sg/rec/model_online/'
 s3_model_online = 's3://algo-rec/rec/model_online/'
-tar_name = 'model_seq_off_1204_test_5.tar.gz'
+tar_name = 'model_seq_off_1204_test_6.tar.gz'
 s3_model_online_tar_file = s3_model_online + tar_name
-endpoint = 'ctr-model-seq-off-1204-5'
+endpoint = 'ctr-model-seq-off-1204-6'
 
 s3_sp_pt_dir = "s3://warehouse-algo/rec/cn_rec_detail_sample_v1/"
 s3_sp_pt_dir_key = "rec/cn_rec_detail_sample_v1/"
