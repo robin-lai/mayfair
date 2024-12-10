6a466bd76ef3a69374964883f39cf57e4f318a7b
dehe.lai <dehe.lai@urbanic.com>
2024-12-10 19:49:32 +0800

    sample

--- algo_rec/recall/swing_multi_process.py
+++ algo_rec/recall/swing_multi_process.py
@@ -132,7 +132,7 @@ def swing(*args):
                 vs.append(ele[0] + chr(4) + str(ele[1]))
             line = (c + chr(4) + trig + chr(1) + chr(2).join(vs) + '\n')
             lines.append(line)
-        fout.writelines(line)
+        fout.writelines(lines)
     os.system('aws s3 cp %s %s' % (out_file, s3_file))
 
 
