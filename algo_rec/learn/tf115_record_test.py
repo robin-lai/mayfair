0a511d64b0f2e48d729b7452e938d2508248b87f
dehe.lai <dehe.lai@urbanic.com>
2024-12-18 17:50:19 +0800

    fix

--- algo_rec/learn/tf115_record_test.py
+++ algo_rec/learn/tf115_record_test.py
@@ -57,7 +57,7 @@ def input_fn_from_local_tfrecords(mode, channel=None, feature_description=None, 
     if fn_mode == 'MultiWorkerShard':
         dataset = dataset.shard(num_host, host_rank)
     dataset = dataset.map(_parse_fea, num_parallel_calls=num_parallel_calls)
-    dataset = dataset.filter(lambda x, y: tf.math.equal(x['country'],'IN'))
+    dataset = dataset.filter(lambda x, y: tf.math.equal(x['country'][0],'IN'))
 
     dataset = dataset.shuffle(buffer_size=batch_size * shuffle_factor)
     dataset = dataset.batch(batch_size)
