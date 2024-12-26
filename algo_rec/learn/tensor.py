5cafd897a73706a3df9bf48660311ea9a9a20bfd
dehe.lai <dehe.lai@urbanic.com>
2024-12-26 12:41:30 +0800

    u2i2i

--- algo_rec/learn/tensor.py
+++ algo_rec/learn/tensor.py
@@ -3,7 +3,7 @@ tf.compat.v1.enable_eager_execution()
 
 t = tf.constant([1,2,3,4,5])
 print(t.numpy().tolist())
-print(t.tolist()) # error
+# print(t.tolist()) # error
 
 t = tf.constant([[1], [2],[3]])
 print(t.get_shape())
