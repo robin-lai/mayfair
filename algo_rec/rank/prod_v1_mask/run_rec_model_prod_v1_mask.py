971e83813a5f20bbb2ebae5eea5a1b43d339410a
dehe.lai <dehe.lai@urbanic.com>
2024-12-28 15:41:25 +0800

    v20 savana_in

--- algo_rec/rank/prod_v1_mask/run_rec_model_prod_v1_mask.py
+++ algo_rec/rank/prod_v1_mask/run_rec_model_prod_v1_mask.py
@@ -405,7 +405,7 @@ class DIN(tf.estimator.Estimator):
                 }
                 return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
 
-            loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits),
+            loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits),
                                      name="loss")
             accuracy = tf.metrics.accuracy(labels=labels,
                                                predictions=tf.to_float(tf.greater_equal(prop, 0.5)))
@@ -496,10 +496,88 @@ class DIN(tf.estimator.Estimator):
                 }
                 return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
 
-            loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels['is_clk'], logits=logits),
+            loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels['is_clk'], logits=logits),
                                  name="loss")
-            loss_cvr = tf.reduce_sum(tf.keras.backend.binary_crossentropy(labels['is_pay'], ctcvr),name="loss")
+            loss_cvr = tf.reduce_mean(tf.keras.backend.binary_crossentropy(labels['is_pay'], ctcvr),name="loss")
             loss = tf.add(loss, loss_cvr, name="ctcvr_loss")
+
+            if mode == tf.estimator.ModeKeys.EVAL:
+                return tf.estimator.EstimatorSpec(mode, loss=loss)
+
+            # Create training op.
+            assert mode == tf.estimator.ModeKeys.TRAIN
+            optimizer = tf.train.AdagradOptimizer(learning_rate=params['learning_rate'])
+            train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+            return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+
+        def _model_fn_ctr(features, labels, mode, params):
+            print('current task:', params['task'])
+            print('features:', features)
+            print('labels', labels)
+            print('mode', mode)
+            print('params', params)
+            cate_cols_emb = params["feature_columns"]["cate_cols_emb"]
+            cate_cols_emb_input = tf.feature_column.input_layer(features, cate_cols_emb)
+            numric_cols_emb = params["feature_columns"]["numric_cols_emb"]
+            numric_cols_emb_input = tf.feature_column.input_layer(features, numric_cols_emb)
+            input_layer = [numric_cols_emb_input, cate_cols_emb_input]
+
+            if 'seq_off' in params['version']:
+                seq_goodsid_input = attention_layer(seq_ids=features['seq_goods_id'], tid_ids=features['goods_id'],
+                                                    id_type='seq_off_goods_id', shape=[40000, 32])
+                seq_cateid_input = attention_layer(seq_ids=features['seq_cate_id'], tid_ids=features['cate_id'],
+                                                   id_type='seq_off_cate_id', shape=[2000, 16])
+                input_layer.extend([seq_goodsid_input, seq_cateid_input])
+
+            if 'seq_mask_on' in params['version']:
+                seq_high_on_goodsid_input = attention_layer_mask(seq_ids=features['highLevelSeqListGoods'], tid_ids=features['goods_id'],
+                                                         id_type='seq_on_high_goods_id', shape=[40000, 32], att_type='dot'
+                                                         , seq_len_actual=features['highLevelSeqList_len'], max_len=20,initialize=params['initialize'])
+                seq_high_on_cateid_input = attention_layer_mask(seq_ids=features['highLevelSeqListCateId'], tid_ids=features['cate_id'],
+                                                                 id_type='seq_on_high_cate_id', shape=[2000, 16], att_type='dot'
+                                                                 , seq_len_actual=features['highLevelSeqList_len'], max_len=20,initialize=params['initialize'])
+
+                seq_low_on_goodsid_input = attention_layer_mask(seq_ids=features['lowerLevelSeqListGoods'], tid_ids=features['goods_id'],
+                                                            id_type='seq_on_low_goods_id', shape=[40000, 32], att_type='dot'
+                                                                 , seq_len_actual=features['lowerLevelSeqList_len'], max_len=20,initialize=params['initialize'])
+                seq_low_on_cateid_input = attention_layer_mask(seq_ids=features['lowerLevelSeqListCateId'], tid_ids=features['cate_id'],
+                                                           id_type='seq_on_low_cate_id', shape=[2000, 16], att_type='dot'
+                                                                 , seq_len_actual=features['lowerLevelSeqList_len'], max_len=20,initialize=params['initialize'])
+                input_layer.extend([seq_high_on_cateid_input,seq_high_on_goodsid_input, seq_low_on_cateid_input, seq_low_on_goodsid_input])
+            elif 'seq_on' in params['version']:
+                seq_high_on_goodsid_input = attention_layer(seq_ids=features['highLevelSeqListGoods'], tid_ids=features['goods_id'],
+                                                            id_type='seq_on_high_goods_id', shape=[40000, 32])
+                seq_high_on_cateid_input = attention_layer(seq_ids=features['highLevelSeqListCateId'], tid_ids=features['cate_id'],
+                                                           id_type='seq_on_high_cate_id', shape=[2000, 16])
+                seq_low_on_goodsid_input = attention_layer(seq_ids=features['lowerLevelSeqListGoods'], tid_ids=features['goods_id'],
+                                                           id_type='seq_on_low_goods_id', shape=[40000, 32])
+                seq_low_on_cateid_input = attention_layer(seq_ids=features['lowerLevelSeqListCateId'], tid_ids=features['cate_id'],
+                                                          id_type='seq_on_low_cate_id', shape=[2000, 16])
+                input_layer.extend([seq_high_on_cateid_input,seq_high_on_goodsid_input, seq_low_on_cateid_input, seq_low_on_goodsid_input])
+
+            # input_layer = [numric_cols_emb_input, cate_cols_emb_input]
+            for ele in input_layer:
+                print('blick layer shape:', ele.get_shape())
+            net = tf.concat(input_layer, axis=1)
+            print('input net shape:', net.get_shape())
+            with tf.variable_scope('ctr_model'):
+                for units in params['hidden_units']:
+                    net = tf.layers.dense(net, units=units, activation=tf.nn.relu)
+                logits = tf.layers.dense(net, units=1)
+                prop = tf.sigmoid(logits, name="pred")
+            if mode == tf.estimator.ModeKeys.PREDICT:
+                print('modekeys predict:', mode)
+                predictions = {
+                    'ctr': prop,
+                    'cvr': prop
+                }
+                export_outputs = {
+                    'prediction': tf.estimator.export.PredictOutput(predictions)
+                }
+                return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
+
+            loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels['is_clk'], logits=logits),
+                                 name="loss")
 
             if mode == tf.estimator.ModeKeys.EVAL:
                 return tf.estimator.EstimatorSpec(mode, loss=loss)
@@ -517,6 +595,14 @@ class DIN(tf.estimator.Estimator):
             else:
                 super(DIN, self).__init__(
                     model_fn=_model_fn_esmm, model_dir=model_dir, config=config, params=params,
+                    warm_start_from=warm_start_from)
+        elif params['task'] == 'ctr':
+            if warm_start_from is None:
+                super(DIN, self).__init__(
+                    model_fn=_model_fn_ctr, model_dir=model_dir, config=config, params=params)
+            else:
+                super(DIN, self).__init__(
+                    model_fn=_model_fn_ctr, model_dir=model_dir, config=config, params=params,
                     warm_start_from=warm_start_from)
         else:
             if warm_start_from is None:
