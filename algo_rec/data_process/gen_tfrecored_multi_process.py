2687effe5cd40f5f2498f55c5f33a98fe37796cd
dehe.lai <dehe.lai@urbanic.com>
2024-12-31 09:43:32 +0800

    D

--- algo_rec/data_process/gen_tfrecored_multi_process.py
+++ algo_rec/data_process/gen_tfrecored_multi_process.py
@@ -219,8 +219,9 @@ def run_multi_process(func,args):
     [p.start() for p in proc_list]
     [p.join() for p in proc_list]
     fail_cnt = sum([p.exitcode for p in proc_list])
-    if fail_cnt:
-        raise ValueError('Failed in %d process.' % fail_cnt)
+    if fail_cnt > 0:
+        print('process fail cnt:', fail_cnt)
+        # raise ValueError('Failed in %d process.' % fail_cnt)
 
 def get_item_feature(file):
     ret = {}
