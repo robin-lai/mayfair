1d7f971ec1b44c51d7decf72acb6542faa7643ae
dehe.lai <dehe.lai@urbanic.com>
2025-01-01 08:56:07 +0800

    Update

--- algo_rec/rank/file_copy.py
+++ algo_rec/rank/file_copy.py
@@ -1,23 +1,26 @@
 import os
 import argparse
 import time
+import multiprocessing
+
+def process(ll):
+    os.system("aws s3 cp --recursive %s  %s" % (ll[0], ll[1]))
+
 
 def main(args):
-    from_dir = "s3://warehouse-algo/rec/cn_rec_detail_sample_v20_savana_in_tfr/ds=%s/" % args.ds
-    to_dir = "s3://warehouse-algo/rec/cn_rec_detail_sample_v20_savana_in_tfr/ds=20241210-20241216/"
-    os.system("aws s3 cp --recursive %s  %s" % (from_dir, to_dir))
+    ll = [[args.from_dir % ds, args.to_dir] for ds in args.range.split(',')]
+    proc_list = [multiprocessing.Process(target=process, args=t) for t in ll]
+    [p.start() for p in proc_list]
+    [p.join() for p in proc_list]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        prog='deploy',
-        description='deploy',
-        epilog='deploy')
+        prog='file_copy',
+        description='file_copy',
+        epilog='file_copy')
     parser.add_argument('--range', type=str, default='20241211,20241212,20241213,20241214,20241215,20241216')
+    parser.add_argument('--from_dir', type=str, default='s3://warehouse-algo/rec/cn_rec_detail_sample_v20_savana_in_tfr/ds=%s/')
+    parser.add_argument('--to_dir', type=str, default='s3://warehouse-algo/rec/cn_rec_detail_sample_v20_savana_in_tfr/ds=20241210-20241216/')
     args = parser.parse_args()
-    for ds in args.range.split(','):
-        st = time.time()
-        args.ds = ds
-        main(args)
-        ed = time.time()
-        print('cp %s cost %s' % (str(args.ds), str(ed-st)))
+    main(args)
