21c45d6e82517a9be884b1ce96c89df60a7bbef2
dehe.lai <dehe.lai@urbanic.com>
2025-01-02 15:32:18 +0800

    D

--- algo_rec/deploy/prod_v1_mask/deploy.py
+++ algo_rec/deploy/prod_v1_mask/deploy.py
@@ -270,7 +270,7 @@ def update_edp(args):
     endpoint_info = sm_cli.describe_endpoint(EndpointName=args.endpoint)
     print(endpoint_info)
     model_name = args.model_name + '_' + args.edp_version
-    model_name = model_name.model_name.replace('_', '-')
+    model_name = model_name.replace('_', '-')
     variant_name = "Variant-xl-%s" % model_name
     endpoint_config_name = 'cf-%s' % model_name
 
