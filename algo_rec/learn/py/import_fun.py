5709fdec7595124fd8f7d42af38ed316c1bc3c1e
dehe.lai <dehe.lai@urbanic.com>
2025-01-03 16:33:48 +0800

    D

--- algo_rec/learn/py/import_fun.py
+++ algo_rec/learn/py/import_fun.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
-sys.path.append(str(Path(__file__).absolute().parent.parent))
+sys.path.append(str(Path(__file__).absolute().parent.parent.parent))
+
 from algo_rec.utils.util import add_job_monitor
 if __name__ == '__main__':
     add_job_monitor(1,1,1)
