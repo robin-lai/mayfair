bc19f263bf558a90cee82c798cb2b54f78656e11
dehe.lai <dehe.lai@urbanic.com>
2025-01-10 16:30:02 +0800

    D

--- pkg_code.py
+++ pkg_code.py
@@ -1,6 +1,4 @@
 import os
-os.system('cd algo_rec')
 os.system('git pull')
-os.system('cd ..')
 os.system('tar -cvf algo_rec.tar.gz algo_rec')
 os.system('aws s3 cp algo_rec.tar.gz s3://warehouse-algo/rec/code/algo_rec.tar.gz')
