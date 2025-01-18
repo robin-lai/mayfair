e131796f138fd0bd0816cda1ad0df25d8b32852a
dehe.lai <dehe.lai@urbanic.com>
2025-01-18 22:53:42 +0800

    mt D

--- algo_rec/data_process/fun_test.py
+++ algo_rec/data_process/fun_test.py
@@ -2,3 +2,4 @@ from gen_tfrecored_multi_process_with_gen_mt import get_hot_i2leaf,get_site_hot
 
 hot_i2leaf = get_hot_i2leaf('s3://warehouse-algo/rec/recall/cn_rec_detail_recall_main_leaf2i_ds/ds=20250113/')
 site_hot = get_site_hot('s3://warehouse-algo/rec/recall/cn_rec_detail_recall_site_hot/ds=20250113/')
+print(site_hot)
