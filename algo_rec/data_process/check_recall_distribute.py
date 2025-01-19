bccad5c3a83dd1daf43c81d85c0d16111ff2ec8e
dehe.lai <dehe.lai@urbanic.com>
2025-01-19 20:47:04 +0800

    mt D

--- algo_rec/data_process/check_recall_distribute.py
+++ algo_rec/data_process/check_recall_distribute.py
@@ -22,15 +22,18 @@ def get_bucket_files(local_dir, buket):
     return ll
 
 def recall_ana(d):
-    n =  len(d['sample_id'])
-    print('n:',n)
     recall = {}
-    for ss in d['s']:
-        for s in ss:
-           if s in recall:
-               recall[s] += 1
-           else:
-               recall[s] = 0
+    n = 0
+    for i in d.keys():
+        s_len = len(d[i]['s'])
+        n += s_len
+        print(f"s_len:{s_len}")
+        for ss in d[i]['s']:
+            for s in ss:
+               if s in recall:
+                   recall[s] += 1
+               else:
+                   recall[s] = 0
     for k, v in recall.items():
         print(f"s:{k}, ratio:{v / n}")
 
@@ -38,9 +41,9 @@ def recall_ana(d):
 def main(args):
     file_ll = get_bucket_files('./tmp/', args.stat_file)
     d = {}
-    for file in file_ll[1:]: # first is base dir
+    for i, file in enumerate(file_ll[1:]): # first is base dir
         with open(file, 'rb') as fin:
-            d.update(pickle.load(fin))
+            d[i] = pickle.load(fin)
     recall_ana(d)
 
 
