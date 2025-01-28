1c72203d692445e3b41ae5fb607a7ed899459337
dehe.lai <dehe.lai@urbanic.com>
2025-01-28 14:55:49 +0800

    M

--- algo_rec/rank/file_copy.py
+++ algo_rec/rank/file_copy.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
         prog='file_copy',
         description='file_copy',
         epilog='file_copy')
-    parser.add_argument('--range', type=str, default='20250101,20250102,20250103,20250104,20250105,20250106,20250107,20250108,20250109,20250110,20250111,20250112,20250113,20250114,20250115,20250116,20250117,20250118,20250119')
-    parser.add_argument('--from_dir', type=str, default='s3://warehouse-algo/rec/cn_rec_detail_sample_v30_savana_in_tfr/ds=%s/')
-    parser.add_argument('--to_dir', type=str, default='s3://warehouse-algo/rec/cn_rec_detail_sample_v30_savana_in_tfr/ds=20250101-20250119/')
+    parser.add_argument('--range', type=str, default='20250124,20250125,20250126,20250127')
+    parser.add_argument('--from_dir', type=str, default='s3://warehouse-algo/rec/cn_rec_detail_sample_v30_savana_in_tfr_bak/ds=%s/')
+    parser.add_argument('--to_dir', type=str, default='s3://warehouse-algo/rec/cn_rec_detail_sample_v30_savana_in_tfr/ds=20250124-20250127/')
     args = parser.parse_args()
     main(args)
