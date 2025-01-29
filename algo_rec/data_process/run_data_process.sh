1eeeb2672d73ddd32c0749d0a38138c534fb96d8
dehe.lai <dehe.lai@urbanic.com>
2025-01-29 09:02:26 +0800

    M

--- algo_rec/data_process/run_data_process.sh
+++ algo_rec/data_process/run_data_process.sh
@@ -1,7 +1,6 @@
-if [ $1 = "tfr" ]; then
-    python gen_tfrecored_multi_process.py --range=202501
-
-elif [ $1 = "tfr_mt" ]; then
+if [ $1 = "tfr_mt" ]; then
+    python gen_tfrecored_multi_process_with_gen_mt.py
+elif [ $1 = "other" ]; then
     python run_rec_model_sg_di.py  --instance_count=1 --warm_start_from=NEW --train_ds=20250119eval
 
 fi
