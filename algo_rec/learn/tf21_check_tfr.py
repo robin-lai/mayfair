0dc6853a720664d68735dafff30bc56a9cc06c9e
dehe.lai <dehe.lai@urbanic.com>
2025-01-29 22:50:28 +0800

    M check auc

--- algo_rec/learn/tf21_check_tfr.py
+++ algo_rec/learn/tf21_check_tfr.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import tensorflow.compat.v1 as v1
+from babel.messages.frontend import extract_messages
 
 tfrecord_file = "part-00000-4e93eb14-9da8-475f-b889-7ff7ca761cfe-c000"
 
@@ -14,12 +15,24 @@ for raw_record in raw_dataset.take(5):  # 读取前5条记录
         print(f"Error parsing record: {e}")
 
 
-example = tf.train.Example(features=tf.train.Features(feature={
-    "mt": tf.train.Feature(bytes_list=tf.train.BytesList(value=[b"some_data"])),
-}))
+# example = tf.train.Example(features=tf.train.Features(feature={
+#     "mt": tf.train.Feature(bytes_list=tf.train.BytesList(value=[b"some_data"])),
+# }))
+feature = {}
+def bytes_fea(v_list, n=1, encode=False):
+    v_list = v_list if isinstance(v_list, list) else [v_list]
+    if len(v_list) > n:
+        v_list = v_list[:n]
+    elif len(v_list) < n:
+        v_list.extend([""] * (n - len(v_list)))
+    return tf.train.Feature(bytes_list=tf.train.BytesList(
+        value=[bytes(v, encoding="utf8") for v in v_list]))
+feature["mt"] = bytes_fea(["i2i", "u2i"], n=3)
+sample = tf.train.Example(features=tf.train.Features(feature=feature))
+record = sample.SerializeToString()
 
 with tf.io.TFRecordWriter("output.tfrecord") as writer:
-    writer.write(example.SerializeToString())
+    writer.write(record)
 
     def parse(data):
         feature_describe = {
