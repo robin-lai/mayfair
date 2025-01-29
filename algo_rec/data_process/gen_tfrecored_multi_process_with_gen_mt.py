bf96bd21b9dd83cb5836c576c7632bd0d6b1d602
dehe.lai <dehe.lai@urbanic.com>
2025-01-29 08:51:34 +0800

    M

--- algo_rec/data_process/gen_tfrecored_multi_process_with_gen_mt.py
+++ algo_rec/data_process/gen_tfrecored_multi_process_with_gen_mt.py
@@ -539,7 +539,7 @@ if __name__ == '__main__':
                         default=(datetime.today() - timedelta(days=2)).strftime('%Y%m%d'))
     parser.add_argument('--debug', type=bool, default=False)
     parser.add_argument('--range', type=str, default='')
-    parser.add_argument('--thread', type=int, default=12)
+    parser.add_argument('--thread', type=int, default=14)
     parser.add_argument('--sample_num', type=int, default=None)
     parser.add_argument('--dir_pt', default='cn_rec_detail_sample_v20_savana_in/ds=%s')
     parser.add_argument('--dir_tfr', default='cn_rec_detail_sample_v30_savana_in_tfr/ds=%s')
