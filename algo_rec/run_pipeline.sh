77f70e8d214d1f85f929e59e0bdce300c3fe647e
dehe.lai <dehe.lai@urbanic.com>
2025-01-30 11:56:04 +0800

    M check auc

--- algo_rec/run_pipeline.sh
+++ algo_rec/run_pipeline.sh
@@ -7,8 +7,9 @@ elif [ $1 = "train_d" ]; then
     python ./rank/prod_v2/run_rec_model_sg_di.py  --instance_count=1 --warm_start_from=NEW --train_ds=20250119eval
 
 elif [ $1 = "pred_d" ]; then
-#    python ./rank/prod_v2/predict_tfr_mtl_v2.py --model_name=mtl_seq_esmm_v2 --ds=20250101-20250119 --proc=1 --sample_num=1
-    python ./rank/prod_v2/predict_tfr_mtl_v2.py --model_name=mtl_seq_esmm_v2 --ds=20241218-20241231 --proc=1 --sample_num=1
+    python ./rank/prod_v2/predict_tfr_mtl_v2.py --model_name=mtl_seq_esmm_v2 --ds=20250120-20250126 --pred_ds=20250129 --proc=1 --sample_num=1
+elif [ $1 = "pred_d_pdb" ]; then
+    python -m pdb ./rank/prod_v2/predict_tfr_mtl_v2.py --model_name=mtl_seq_esmm_v2 --ds=20250120-20250126 --pred_ds=20250129 --proc=1 --sample_num=1
 elif [ $1 = "pred" ]; then
     python ./rank/prod_v2/predict_tfr_mtl_v2.py --model_name=mtl_seq_esmm_v2 --ds=20250120-20250126 --pred_ds=20250129
 
