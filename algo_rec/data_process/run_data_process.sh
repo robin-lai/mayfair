9b760e5e628c1a523d435291dd8edfc06864570c
dehe.lai <dehe.lai@urbanic.com>
2025-02-05 22:45:17 +0800

    prod_v3

--- algo_rec/data_process/run_data_process.sh
+++ algo_rec/data_process/run_data_process.sh
@@ -1,6 +1,5 @@
 if [ $1 = "tfr_mt" ]; then
     python gen_tfrecored_multi_process_with_gen_mt.py
-elif [ $1 = "other" ]; then
-    python run_rec_model_sg_di.py  --instance_count=1 --warm_start_from=NEW --train_ds=20250119eval
-
+elif [ $1 = "mt_ana" ]; then
+    python check_recall_distribute.py --range='' --ds=20250126
 fi
