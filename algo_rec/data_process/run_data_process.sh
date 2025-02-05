3236ccce4e173672f11e748ccd346bc896d2c5d8
dehe.lai <dehe.lai@urbanic.com>
2025-02-05 23:43:34 +0800

    prod_v3

--- algo_rec/data_process/run_data_process.sh
+++ algo_rec/data_process/run_data_process.sh
@@ -1,5 +1,7 @@
 if [ $1 = "tfr_mt" ]; then
     python gen_tfrecored_multi_process_with_gen_mt.py --ds=20250120
+elif [ $1 = "tfr_mt_d" ]; then
+    python gen_tfrecored_multi_process_with_gen_mt.py --ds=20250120 --sample_num=1 --thread=1
 elif [ $1 = "mt_ana" ]; then
     python check_recall_distribute.py --range='' --ds=20250126
 elif [ $1 = "mt_ana_range" ]; then
