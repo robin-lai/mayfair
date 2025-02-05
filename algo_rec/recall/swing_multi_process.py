64f07cf64b699a559c10bf4120ac18ce95d0ff7f
dehe.lai <dehe.lai@urbanic.com>
2025-02-05 22:03:44 +0800

    prod_v3

--- algo_rec/recall/swing_multi_process.py
+++ algo_rec/recall/swing_multi_process.py
@@ -464,13 +464,3 @@ if __name__ == '__main__':
         alert_feishu(f"swing_i2i_recll complete ds:{args.pre_ds}")
 
 
-# nohup python -u swing_multi_process.py --pre_ds=20250106 --beta=0.6 done
-# nohup python -u swing_multi_process.py --pre_ds=20250106 --beta=0.7 done
-# nohup python -u swing_multi_process.py --pre_ds=20250107 --beta=0.7 --ubeta=0.5 --alph=1.0 --p=10  cpu vs mem: 6:8 r5.4x  6440s
-# nohup python -u swing_multi_process.py --pre_ds=20250108,20250109,20250110,20250111,20250112,20250113,20250114,20250115,20250116 --beta=0.7 --ubeta=0.5 --alph=1.0 --p=10 done
-# nohup python -u swing_multi_process.py --pre_ds=20241228,20241229,20241230,20241231,20250101,20250102,20250103,20250104,20250105 --beta=0.7 --ubeta=0.5 --alph=1.0 --p=8 run
-# nohup python -u swing_multi_process.py --pre_ds=20241217,20241218,20241219,20241220,20241221,20241222,20241223 --beta=0.7 --ubeta=0.5 --alph=1.0 --p=10 done
-# nohup python -u swing_multi_process.py --pre_ds=20241222,20241223,20241224,20241225,20241226,20241227 --beta=0.7 --ubeta=0.5 --alph=1.0 --p=8 wait
-# error ds 20241228
-# 10内存会满
-# nohup python -u swing_multi_process.py --pre_ds=20250117,20250118,20250119 --beta=0.7 --ubeta=0.5 --alph=1.0 --p=7
