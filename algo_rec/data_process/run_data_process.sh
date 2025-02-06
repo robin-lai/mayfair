7c4ba931fbeb72a56f810555673999dbf2b6d39f
dehe.lai <dehe.lai@urbanic.com>
2025-02-06 22:28:11 +0800

    v4

--- algo_rec/data_process/run_data_process.sh
+++ algo_rec/data_process/run_data_process.sh
@@ -13,6 +13,6 @@ elif [ $1 = "mt_ana" ]; then
     python check_recall_distribute.py --range='' --ds=20250120
 elif [ $1 = "mt_ana_pos_idx" ]; then
     python check_recall_distribute_pos_idx.py --range='' --ds=20250120
-elif [ $1 = "mt_ana_range" ]; then
-    python check_recall_distribute.py --range='20250101,20250102,20250103,20250104,20250105,20250106,20250107,20250108,20250109,20250110,20250111,20250112,20250113,20250114,20250115,20250116,20250117,20250118,20250119'
+elif [ $1 = "mt_ana_range_pos_idx" ]; then
+    python check_recall_distribute_pos_idx.py
 fi
