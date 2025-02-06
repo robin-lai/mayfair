9e8c2ff00dfefe9b2460c4d090629db2fb452792
dehe.lai <dehe.lai@urbanic.com>
2025-02-06 22:40:20 +0800

    v4

---
+++ algo_rec/rank/file_copy2.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+import time
+import multiprocessing
+
+def process(from_dir, to_dir):
+    os.system("aws s3 cp --recursive %s  %s" % (from_dir, to_dir))
+
+
+def main(args):
+    ll = [[args.from_dir % ds, args.to_dir] for ds in args.range.split(',')]
+    proc_list = [multiprocessing.Process(target=process, args=t) for t in ll]
+    [p.start() for p in proc_list]
+    [p.join() for p in proc_list]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='file_copy',
+        description='file_copy',
+        epilog='file_copy')
+    parser.add_argument('--range', type=str, default='20250129,20250130,20250131,20250201,20250202,20250203,20250204,20250205')
+    parser.add_argument('--from_dir', type=str, default='s3://warehouse-algo/rec/cn_rec_detail_sample_v30_savana_in_tfr_row_n300/ds=%s/')
+    parser.add_argument('--to_dir', type=str, default='s3://warehouse-algo/rec/cn_rec_detail_sample_v30_savana_in_tfr_row_n300/ds=20250129-20250205/')
+    args = parser.parse_args()
+    main(args)
