699531f079824e84390a38db19a5d5aa635e49c4
dehe.lai <dehe.lai@urbanic.com>
2025-02-07 08:54:41 +0800

    v4

--- algo_rec/rank/prod_v4/run.sh
+++ algo_rec/rank/prod_v4/run.sh
@@ -1,6 +1,6 @@
 if [ $1 = "train" ]; then
-    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v4 --instance_count=3 --warm_start_from=NEW --train_ds=20250120-20250128 --eval_ds=20250120eval --sample=cn_rec_detail_sample_v30_savana_in_tfr_row_n300
-    sleep 600
+#    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v4 --instance_count=3 --warm_start_from=NEW --train_ds=20250120-20250128 --eval_ds=20250120eval --sample=cn_rec_detail_sample_v30_savana_in_tfr_row_n300
+#    sleep 600
     python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v4 --instance_count=3 --warm_start_from=NEWEST --train_ds=20250129-20250204 --pre_ds=20250120-20250128 --eval_ds=20250120eval --sample=cn_rec_detail_sample_v30_savana_in_tfr_row_n300
 
 elif [ $1 = "train_d" ]; then
