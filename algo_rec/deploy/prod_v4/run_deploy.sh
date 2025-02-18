6edf5b986f869048fbb8944083e07251a621a034
dehe.lai <dehe.lai@urbanic.com>
2025-02-18 16:57:55 +0800

    prod_v4_deploy

--- algo_rec/deploy/prod_v4/run_deploy.sh
+++ algo_rec/deploy/prod_v4/run_deploy.sh
@@ -1,5 +1,7 @@
 if [ $1 = "pkg" ]; then
     python deploy.py --pipeline=pkg  --model_name=mtl_seq_esmm_v4  --edp_version=20250129-20250204
 elif [ $1 = "edp" ]; then
-    python deploy.py --pipeline=pkg  --model_name=mtl_seq_esmm_v4  --edp_version=20250129-20250204
+    python deploy.py --pipeline=edp  --model_name=mtl_seq_esmm_v4  --edp_version=20250129-20250204
+elif [ $1 = "req_sg" ]; then
+    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v4  --edp_version=20250129-20250204
 fi
