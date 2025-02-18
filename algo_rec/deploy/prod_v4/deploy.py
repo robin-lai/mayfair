28fcd0c295df4015ac4de48dece57d286b8f325c
dehe.lai <dehe.lai@urbanic.com>
2025-02-18 17:10:33 +0800

    prod_v4_deploy

--- algo_rec/deploy/prod_v4/deploy.py
+++ algo_rec/deploy/prod_v4/deploy.py
@@ -56,7 +56,9 @@ request = {
         "1477842"
     ],
     "itemContextMap": {
-        "1402902": {"s": "i2i_long,i2i_short,hot,hot_i2leaf,i2i_main"}, "1327692": {"s": ""}
+        "1327692": {"s": "i2i_long,i2i_short,hot,hot_i2leaf,i2i_main", "ss": "0.1,0.2,0.3,0.4,0.5"}, "1402902": {"s": "","ss":""},
+        "1459992": {"s": "i2i_long,i2i_short,hot,hot_i2leaf,i2i_main", "ss": "0.1,0.2,0.3,0.4,0.5"},
+        "1477842": {"s": "", "ss": ""}
     },
     "ip": "127.0.0.1",
     "platform": "H5",
