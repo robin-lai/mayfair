150d2bfa49306950c0d98ca8f6ad89b034f9839c
dehe.lai <dehe.lai@urbanic.com>
2025-02-18 10:17:08 +0800

    v4

--- algo_rec/data_process/run_data_process.sh
+++ algo_rec/data_process/run_data_process.sh
@@ -2,11 +2,11 @@ if [ $1 = "tfr_mt" ]; then
     python gen_tfrecored_multi_process_with_gen_mt.py --ds=20250120 --stat_flag=True
 elif [ $1 = "tfr_mt_range" ]; then
 #  20250120,20250121,20250122,20250130,20250131
-    python gen_tfrecored_multi_process_with_gen_mt.py --range=20250201,20250202,20250203,20250204,20250205 --stat_flag=True
+    python gen_tfrecored_multi_process_with_gen_mt.py --range=20250206,20250207,20250208,20250209,20250210
 elif [ $1 = "tfr_mt_range2" ]; then
-    python gen_tfrecored_multi_process_with_gen_mt.py --range=20250123,20250124,20250125,20250126 --stat_flag=True
+    python gen_tfrecored_multi_process_with_gen_mt.py --range=20250211,20250212,20250213,20250214
 elif [ $1 = "tfr_mt_range3" ]; then
-    python gen_tfrecored_multi_process_with_gen_mt.py --range=20250127,20250128,20250129 --stat_flag=True
+    python gen_tfrecored_multi_process_with_gen_mt.py --range=20250216,20250217,20250218,20250215
 elif [ $1 = "tfr_mt_d" ]; then
     python gen_tfrecored_multi_process_with_gen_mt.py --ds=20250120 --sample_num=1 --thread=1
 elif [ $1 = "mt_ana" ]; then
