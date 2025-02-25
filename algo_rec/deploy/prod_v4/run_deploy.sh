553f59a3ddcb9bb69aff0641e71d3325093026e0
dehe.lai <dehe.lai@urbanic.com>
2025-02-25 10:00:37 +0800

    prod_v4_deploy

--- algo_rec/deploy/prod_v4/run_deploy.sh
+++ algo_rec/deploy/prod_v4/run_deploy.sh
@@ -1,9 +1,9 @@
 if [ $1 = "pkg" ]; then
-    python deploy.py --pipeline=pkg  --model_name=mtl_seq_esmm_v4  --edp_version=20250205-20250217  --region=sg --debug_v=5
+    python deploy.py --pipeline=pkg  --model_name=mtl_seq_esmm_v4  --edp_version=20250205-20250217  --region=in 
 elif [ $1 = "edp" ]; then
-    python deploy.py --pipeline=edp  --model_name=mtl_seq_esmm_v4  --edp_version=20250205-20250217 --region=sg --debug_v=5
+    python deploy.py --pipeline=edp  --model_name=mtl_seq_esmm_v4  --edp_version=20250205-20250217 --region=in 
 elif [ $1 = "local" ]; then # change base_data_dir on inference.py
     python inference.py
 elif [ $1 = "req_sg" ]; then
-    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v4  --edp_version=20250205-20250217  --region=sg --debug_v=5 --debug=log
+    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v4  --edp_version=20250205-20250217  --region=in  --debug=log
 fi
