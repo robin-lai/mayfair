dcee39ef813659c1214f6f68dcf1242d0801d102
dehe.lai <dehe.lai@urbanic.com>
2025-03-03 17:22:29 +0800

    v2

--- algo_rec/deploy/prod_v2/run_deploy.sh
+++ algo_rec/deploy/prod_v2/run_deploy.sh
@@ -11,7 +11,10 @@ elif [ $1 = "update" ]; then
 elif [ $1 = "local" ]; then # change base_data_dir on inference.py
     python inference.py
 elif [ $1 = "req_sg" ]; then
-    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v2  --edp_version=20250302 --region=sg  --debug=log 
+    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v2  --edp_version=20250302 --region=sg
+    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v2  --edp_version=20250302 --region=in
+elif [ $1 = "req_sg_log" ]; then
+    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v2  --edp_version=20250302 --region=sg  --debug=log
     python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v2  --edp_version=20250302 --region=in --debug=log
 elif [ $1 = "req_sg1" ]; then
     python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v2  --edp_version=20250302 --region=in  --debug=1
