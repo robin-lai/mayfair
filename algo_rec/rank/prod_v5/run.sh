e3ebcb51440639fd882ba8e1d6cb5af603709107
dehe.lai <dehe.lai@urbanic.com>
2025-03-05 17:21:22 +0800

    v5

--- algo_rec/rank/prod_v5/run.sh
+++ algo_rec/rank/prod_v5/run.sh
@@ -1,8 +1,8 @@
 if [ $1 = "train" ]; then
-    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=3 --warm_start_from=NEW --train_ds=20250120-20250128 --eval_ds=20250120eval 
+    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=3 --warm_start_from=NEW --train_ds=20250120-20250128 --eval_ds=20250304eval 
 
 elif [ $1 = "train_d" ]; then
-    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=1 --warm_start_from=NEW --train_ds=20250120eval --eval_ds=20250120eval 
+    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=1 --warm_start_from=NEW --train_ds=20250304eval --eval_ds=20250304eval 
 
 elif [ $1 = "pred_d" ]; then
 #    python predict_tfr_mtl_v2.py --model_name=mtl_seq_esmm_v5 --ds=20250101-20250119 --proc=1 --sample_num=1
