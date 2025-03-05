7ccb7350200330ad2fd40d7008ff620e02c69d50
dehe.lai <dehe.lai@urbanic.com>
2025-03-05 19:12:27 +0800

    v5

--- algo_rec/data_process/run_data_process.sh
+++ algo_rec/data_process/run_data_process.sh
@@ -5,11 +5,11 @@ elif [ $1 = "tfr_mt_ss" ]; then
 elif [ $1 = "tfr_mt_range" ]; then
     python gen_tfrecored_multi_process_with_gen_mt.py --range=20250228,20250301,20250302
 elif [ $1 = "tfr_mt_range_ss" ]; then
-    python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --range=20250301,20250302,20250303
+    python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --range=20250215,20250216,20250217,20250218,20250219
 elif [ $1 = "tfr_mt_range2_ss" ]; then
-    python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --thread=23 --range=20250224,20250225,20250226,20250227,20250228
-elif [ $1 = "tfr_mt_range3" ]; then
-    python gen_tfrecored_multi_process_with_gen_mt.py --range=20250216,20250217,20250218,20250215
+    python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --range=20250224,20250225,20250226,20250227,20250228
+elif [ $1 = "tfr_mt_range3_ss" ]; then
+    python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --range=20250220,20250221,20250222,20250223
 elif [ $1 = "tfr_mt_d" ]; then
     python gen_tfrecored_multi_process_with_gen_mt.py --ds=20250120 --sample_num=1 --thread=1
 elif [ $1 = "tfr_mt_d_ss" ]; then
