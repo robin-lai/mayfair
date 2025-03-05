ffe03982ad84c3f4bbb9c661edacc448b73890da
dehe.lai <dehe.lai@urbanic.com>
2025-03-05 16:20:15 +0800

    v2

--- algo_rec/rank/prod_v5/check_tfr_data.py
+++ algo_rec/rank/prod_v5/check_tfr_data.py
@@ -2,15 +2,26 @@ import os
 
 import tensorflow as tf
 import argparse
+
 print(tf.__version__)
 import tensorflow.compat.v1 as v1
 from feature_serv_describe_tfv2 import feature_describe_pred
+
+feature_describe_pred.update({"highLevelSeqListGoods": v1.FixedLenFeature(20, tf.string, default_value=[""] * 20),
+                              "highLevelSeqListCateId": v1.FixedLenFeature(20, tf.string, default_value=[""] * 20),
+                              "lowerLevelSeqListGoods": v1.FixedLenFeature(20, tf.string, default_value=[""] * 20),
+                              "lowerLevelSeqListCateId": v1.FixedLenFeature(20, tf.string, default_value=[""] * 20),
+                              "highLevelSeqList_len": v1.FixedLenFeature(1, tf.int64, default_value=0),
+                              "lowerLevelSeqList_len": v1.FixedLenFeature(1, tf.int64, default_value=0)})
+
 
 def main(args):
     def parse(data):
         features = tf.io.parse_single_example(data, features=feature_describe_pred)
         return features
-    s3_file = 's3://warehouse-algo/rec/cn_rec_detail_sample_v30_savana_in_tfr_row_n300_sample_select/ds=%s/%s' % (args.ds, args.file)
+
+    s3_file = 's3://warehouse-algo/rec/cn_rec_detail_sample_v30_savana_in_tfr_row_n300_sample_select/ds=%s/%s' % (
+    args.ds, args.file)
     local_file = './' + args.file
     os.system('aws s3 cp %s %s' % (s3_file, local_file))
     ds = tf.data.TFRecordDataset(local_file)
@@ -34,10 +45,8 @@ if __name__ == '__main__':
         epilog='predict')
     parser.add_argument('--file', default='part-00000-5d8949ad-f96a-4927-9883-57e2e4023168-c000')
     parser.add_argument('--ds', default='20250304')
-    parser.add_argument('--batch_size',type=int, default=64)
-    parser.add_argument('--head',type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--head', type=int, default=10)
     parser.add_argument('--names', default='')
     args = parser.parse_args()
     main(args)
-
-
