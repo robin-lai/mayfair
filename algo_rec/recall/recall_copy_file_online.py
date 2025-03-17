e744cfcab817465223cce9b22e0148f1ceee06c8
dehe.lai <dehe.lai@urbanic.com>
2025-03-17 13:22:44 +0800

    v5

--- algo_rec/recall/recall_copy_file_online.py
+++ algo_rec/recall/recall_copy_file_online.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     parser.add_argument('--ds', type=str,
                         default=(datetime.today() - timedelta(days=1)).strftime('%Y%m%d'))
     parser.add_argument('--from_dir', type=str,
-                        default='s3://warehouse-algo/rec/recall/cn_rec_detail_recall_i2i_for_redis_row_n300/item_user_debias_%s_1.0_0.6_0.5/')
+                        default='s3://warehouse-algo/rec/recall/cn_rec_detail_recall_i2i_for_redis_row_n300/item_user_debias_%s_1.0_0.6_0.5')
     parser.add_argument('--to_dir', type=str, default='s3://algo-sg/rec/cn_rec_detail_recall_i2i_for_redis/')
     parser.add_argument('--part', type=int, default=10)
     args = parser.parse_args()
