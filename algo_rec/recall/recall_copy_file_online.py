78e75e828f327ee3c5c0109c06a4e6afeab54b51
dehe.lai <dehe.lai@urbanic.com>
2025-03-17 13:21:03 +0800

    v5

--- algo_rec/recall/recall_copy_file_online.py
+++ algo_rec/recall/recall_copy_file_online.py
@@ -1,13 +1,12 @@
 import os
 import argparse
-from datetime import datetime,date, timedelta
-
-def process(from_dir, to_dir):
-    os.system("aws s3 cp %s/swing_rec_Savana_IN_part_*  %s  --recursive" % (from_dir, to_dir))
+from datetime import datetime, date, timedelta
 
 
 def main(args):
-    process(args.from_dir, args.to_dir)
+    for i in range(args.part):
+        from_file = '%s/swing_rec_Savana_IN_part_%s' % (args.from_dir, i)
+        os.system("aws s3 cp %s  %s" % (from_file, args.to_dir))
 
 
 if __name__ == '__main__':
@@ -17,8 +16,10 @@ if __name__ == '__main__':
         epilog='file_copy')
     parser.add_argument('--ds', type=str,
                         default=(datetime.today() - timedelta(days=1)).strftime('%Y%m%d'))
-    parser.add_argument('--from_dir', type=str, default='s3://warehouse-algo/rec/recall/cn_rec_detail_recall_i2i_for_redis_row_n300/item_user_debias_%s_1.0_0.6_0.5/')
+    parser.add_argument('--from_dir', type=str,
+                        default='s3://warehouse-algo/rec/recall/cn_rec_detail_recall_i2i_for_redis_row_n300/item_user_debias_%s_1.0_0.6_0.5/')
     parser.add_argument('--to_dir', type=str, default='s3://algo-sg/rec/cn_rec_detail_recall_i2i_for_redis/')
+    parser.add_argument('--part', type=int, default=10)
     args = parser.parse_args()
     args.from_dir = args.from_dir % args.ds
     print(args.from_dir)
