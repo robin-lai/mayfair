1d3c025a73f33802c223f4c0c1e0ffa33953f59b
dehe.lai <dehe.lai@urbanic.com>
2025-03-28 13:46:26 +0800

    D

--- algo_rec/deploy/prod_v4/run_deploy.sh
+++ algo_rec/deploy/prod_v4/run_deploy.sh
@@ -1,16 +1,16 @@
 if [ $1 = "pkg" ]; then
-    python deploy.py --pipeline=pkg  --model_name=mtl_seq_esmm_v4  --edp_version=20250307 --region=in --debug_v='' --ds=20250307
+    python deploy.py --pipeline=pkg  --model_name=mtl_seq_esmm_v4  --edp_version=20250324 --region=in --debug_v='' --ds=20250324
 elif [ $1 = "edp" ]; then
-#    python deploy.py --pipeline=edp  --model_name=mtl_seq_esmm_v4  --edp_version=20250307 --region=in --debug_v='' --ds=20250307
-    python deploy.py --pipeline=edp  --model_name=mtl_seq_esmm_v4  --edp_version=20250307 --region=sg --debug_v='' --ds=20250307 --instance_count=1
+#    python deploy.py --pipeline=edp  --model_name=mtl_seq_esmm_v4  --edp_version=20250324 --region=in --debug_v='' --ds=20250324
+    python deploy.py --pipeline=edp  --model_name=mtl_seq_esmm_v4  --edp_version=20250324 --region=sg --debug_v='' --ds=20250324 --instance_count=1
 elif [ $1 = "update" ]; then
-#    python deploy.py --pipeline=pkg  --model_name=mtl_seq_esmm_v4  --edp_version=20250307 --region=in --debug_v='' --ds=20250307
-    python deploy.py --pipeline=update  --model_name=mtl_seq_esmm_v4  --edp_version=20250307 --region=in --debug_v='' --ds=20250307
+#    python deploy.py --pipeline=pkg  --model_name=mtl_seq_esmm_v4  --edp_version=20250324 --region=in --debug_v='' --ds=20250324
+    python deploy.py --pipeline=update  --model_name=mtl_seq_esmm_v4  --edp_version=20250324 --region=in --debug_v='' --ds=20250324
 elif [ $1 = "local" ]; then # change base_data_dir on inference.py
     python inference.py
 elif [ $1 = "req_sg" ]; then
-    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v4  --edp_version=20250307 --region=sg --debug_v='' --debug=log --ds=20250307
-    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v4  --edp_version=20250307 --region=in --debug_v='' --debug=log --ds=20250307
+    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v4  --edp_version=20250324 --region=sg --debug_v='' --debug=log --ds=20250324
+    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v4  --edp_version=20250324 --region=in --debug_v='' --debug=log --ds=20250324
 elif [ $1 = "req_sg1" ]; then
-    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v4  --edp_version=20250307 --region=in --debug_v='' --debug=1 --ds=20250307
+    python deploy.py --pipeline=req_sg  --model_name=mtl_seq_esmm_v4  --edp_version=20250324 --region=in --debug_v='' --debug=1 --ds=20250324
 fi
