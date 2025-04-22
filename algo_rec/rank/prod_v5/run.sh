056afd8d6b859c1e22adc0199e13d8956e53c70f
dehe.lai <dehe.lai@urbanic.com>
2025-04-22 16:00:09 +0800

    D

--- algo_rec/rank/prod_v5/run.sh
+++ algo_rec/rank/prod_v5/run.sh
@@ -7,9 +7,12 @@ if [ $1 = "train" ]; then
 #    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=2 --warm_start_from=NEWEST --train_ds=20250309 --pre_ds=20250308  --eval_ds=20250304eval
 #    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=3 --warm_start_from=NEWEST --train_ds=20250310-20250323 --pre_ds=20250309  --eval_ds=20250304eval
 #    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=2 --warm_start_from=NEWEST --train_ds=20250324 --pre_ds=20250310-20250323  --eval_ds=20250304eval
-    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=2 --warm_start_from=NEWEST --train_ds=20250325 --pre_ds=20250324  --eval_ds=20250304eval
-    sleep 600
-    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=2 --warm_start_from=NEWEST --train_ds=20250326 --pre_ds=20250325  --eval_ds=20250304eval
+#    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=2 --warm_start_from=NEWEST --train_ds=20250325 --pre_ds=20250324  --eval_ds=20250304eval
+#    sleep 600
+#    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=2 --warm_start_from=NEWEST --train_ds=20250326 --pre_ds=20250325  --eval_ds=20250304eval
+    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=2 --warm_start_from=NEWEST --train_ds=20250327 --pre_ds=20250326  --eval_ds=20250304eval
+    sleep 300
+    python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=2 --warm_start_from=NEWEST --train_ds=20250328 --pre_ds=20250327  --eval_ds=20250304eval
 
 elif [ $1 = "train_d" ]; then
     python run_rec_model_sg_di.py  --model_name=mtl_seq_esmm_v5 --instance_count=1 --warm_start_from=NEW --train_ds=20250304eval --eval_ds=20250304eval 
