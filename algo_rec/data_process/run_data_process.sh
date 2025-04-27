51a2df9960da7607b80ff6f42217e833da50197d
dehe.lai <dehe.lai@urbanic.com>
2025-04-27 09:52:05 +0800

    D

--- algo_rec/data_process/run_data_process.sh
+++ algo_rec/data_process/run_data_process.sh
@@ -1,11 +1,11 @@
 if [ $1 = "tfr_mt" ]; then
     python gen_tfrecored_multi_process_with_gen_mt.py --ds=
 elif [ $1 = "tfr_mt_ss" ]; then
-    python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --ds=
+    python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --ds=$2
 elif [ $1 = "tfr_mt_range" ]; then
     python gen_tfrecored_multi_process_with_gen_mt.py --range=20250324,20250325,20250326
 elif [ $1 = "tfr_mt_range_ss" ]; then
-    python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --range=$2
+    python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --range=20250424,20250425,20250426
 elif [ $1 = "tfr_mt_range2_ss" ]; then
     python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --range=20250411,20250401,20250402,20250403
 elif [ $1 = "tfr_mt_range3_ss" ]; then
