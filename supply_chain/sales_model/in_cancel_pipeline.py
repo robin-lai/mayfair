31df791740a792f6f98985b71e60002038190f5c
dehe.lai <dehe.lai@urbanic.com>
2025-05-16 16:50:05 +0800

    D

--- supply_chain/sales_model/in_cancel_pipeline.py
+++ supply_chain/sales_model/in_cancel_pipeline.py
@@ -1,7 +1,9 @@
 import os
 from common import *
 
+import argparse
 time_delta = 1
+model_num = 10
 train_and_predict_data_path = "sc_forecast_sequence_ts_model_train_and_predict_skc/"
 base_dir = "./data_cancel/"
 os.system('rm -rf %s' % base_dir)
@@ -14,15 +16,16 @@ s3_evaluated_result_path = 's3://warehouse-algo/sequence_model_evaluated_result/
 
 saved_model_path = base_dir + "best_model.pth"
 local_train_data_path = base_dir + "sequence_data.csv"
+local_metrics_path = base_dir + "metrics_skc_diff.csv"
 tmp_path = base_dir + 'tmp.txt'
 local_future_dau_plan_path = base_dir + "savana_future_daus.csv"
 local_evaluated_result_path = base_dir + "evaluated_result.parquet"
 local_predicted_result_path = base_dir + 'output.parquet'
 local_predict_dir = base_dir + 'pred/'
 os.system('mkdir %s' % local_predict_dir)
-model_num = 10
 
-if __name__ == '__main__':
+
+def main(args):
     st1 = time.time()
     st = time.time()
     print('step1:process data')
@@ -67,3 +70,14 @@ if __name__ == '__main__':
     os.system('aws s3 cp %s %s' % (local_evaluated_result_path, s3_evaluated_result_path % yesterday_str))
     print('evalute cost:', time.time() - ed)
     print('all cost[hour]:', (time.time() - st1) / 3600)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='sc_iq',
+        description='sc_iq',
+        epilog='sc-iq-help')
+    parser.add_argument('--pipeline', type=str,
+                        default='init,train,pred,eval,metrics')
+    args = parser.parse_args()
+    main(args)
--- supply_chain/sales_model/iq_cancel_pipeline.py
+++ supply_chain/sales_model/iq_cancel_pipeline.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from common import *
 import argparse
+from pipeline import *
 
 time_delta = 0
 train_and_predict_data_path = "sc_forecast_sequence_ts_model_train_and_predict_skc_iq/"
@@ -31,116 +32,29 @@ local_predict_dir = base_dir + 'pred/'
 os.system('mkdir %s' % local_predict_dir)
 
 
+
+
+
 def main(args):
     st1 = time.time()
-    st = time.time()
     print('step1:process data')
     dc = DataConfig("sales_1d", time_delta)
-    yesterday_str = dc.yesterday.strftime("%Y%m%d")
 
+    # step 1: process data
     if 'init' in args.pipeline:
-        # step 1: process data
-        wait_for_ready(train_and_predict_data_path, dc.yesterday.strftime("%Y%m%d"))
-        ret = list(load_s3_dir(BUCKET, train_and_predict_data_path, [dc.yesterday.strftime("%Y%m%d")], tmp_path))
-        ret = pd.DataFrame(ret)
-        ret = ret[ret["target_date"] >= (dc.today - timedelta(days=250)).strftime("%Y-%m-%d")]
-        print(ret["target_date"].max(), ret["target_date"].min())
-        ret.to_csv(local_train_data_path)
-        del ret
-        ed = time.time()
-        print('process data cost:', ed - st)
+        init(dc,train_and_predict_data_path,local_train_data_path, tmp_path)
 
     if 'train' in args.pipeline:
-        ed = time.time()
-        print('step2: train')
-        dc.init_df(local_train_data_path)
-        train_loader, test_loader = prepare_train_valid_data(dc, (dc.today - timedelta(days=0)).strftime('%Y-%m-%d'),
-                                                             train_and_predict_data_path_smooth)
-        train(dc, train_loader, test_loader, saved_model_path)
-        os.system('aws s3 cp %s %s' % (saved_model_path, s3_saved_model_path % (suffix, yesterday_str)))
-        print('train cost:', time.time() - ed)
+        train(dc)
 
     if 'pred' in args.pipeline:
-        print('step3: pred')
-        ed = time.time()
-        for i in range(0, model_num):
-            download_date = dc.yesterday - timedelta(days=i)
-            download_date = download_date.strftime("%Y%m%d")
-            remote_path = s3_saved_model_path % (suffix, download_date) + "best_model_iq.pth"
-            print('remote_path', remote_path)
-            download_file(remote_path, local_predict_dir + "best_model_%s.pth" % download_date)
-        #
-        predicted_result = daily_predict(dc, local_predict_dir)
-        predicted_result.to_parquet(local_predicted_result_path)
-        os.system('aws s3 cp %s %s' % (local_predicted_result_path, s3_pred_result % (suffix, yesterday_str)))
-        print('pred cost:', time.time() - ed)
+        pred(dc)
 
     if 'eval' in args.pipeline:
-        ed = time.time()
-        print('step4: evalute')
-        evaluate_model(dc, local_evaluated_result_path, local_predict_dir, train_and_predict_data_path_smooth_eval)
-        os.system('aws s3 cp %s %s' % (local_evaluated_result_path, s3_evaluated_result_path % (suffix, yesterday_str)))
-        print('evalute cost:', time.time() - ed)
+        eval(dc)
 
     if 'metrics' in args.pipeline:
-        print('step5: 评测')
-        pred_date_str = '2025-05-11'
-        pred_date = datetime.strptime(pred_date_str, "%Y-%m-%d")
-        pred_df = parquet.read_table(s3_pred_result % (suffix, pred_date_str)).to_pandas().drop_duplicates()
-        print(pred_df.describe())
-        real_df_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc_smooth_iq/ds=%s/' % pred_date_str.replace(
-            '-', '')
-        real_df = parquet.read_table(real_df_file).to_pandas()[
-            ['target_date', 'skc_id', 'sales_1d', 'no_cancel_sales_1d']].astype(
-            {"target_date": str, "skc_id": int, "sales_1d": int, "no_cancel_sales_1d": int})
-        real_df['target_date'] = pd.to_datetime(real_df['target_date'])
-        print(real_df.describe())
-        print(pred_df.head())
-        print(real_df.head())
-        pred_ids = set(pred_df['skc_id'].values)
-        real_ids = set(real_df['skc_id'].values)
-        ids = pred_ids.intersection(real_ids)
-        ret_list = []
-        for id in ids:
-            print('id', id)
-            id_df = pred_df[pred_df['skc_id'] == id]
-            w1_p = id_df[id_df['week_num'] == 1]['predict'].values[0]
-            w2_p = id_df[id_df['week_num'] == 2]['predict'].values[0]
-            w3_p = id_df[id_df['week_num'] == 3]['predict'].values[0]
-            w4_p = id_df[id_df['week_num'] == 4]['predict'].values[0]
-            r_df = real_df[real_df['skc_id'] == id].dropna()
-            end_w1 = pred_date + pd.Timedelta(days=7)
-            end_w2 = pred_date + pd.Timedelta(days=14)
-            end_w3 = pred_date + pd.Timedelta(days=21)
-            end_w4 = pred_date + pd.Timedelta(days=28)
-            w1_r = r_df[r_df['target_date'].between(pred_date, end_w1)]['sales_1d'].values
-            w2_r = r_df[r_df['target_date'].between(pred_date, end_w2)]['sales_1d'].values
-            w3_r = r_df[r_df['target_date'].between(pred_date, end_w3)]['sales_1d'].values
-            w4_r = r_df[r_df['target_date'].between(pred_date, end_w4)]['sales_1d'].values
-            w1_diff, w2_diff, w3_diff, w4_diff = -1, -1, -1, -1
-            w1_r2, w2_r2, w3_r2, r4_r2 = -1, -1, -1, -1
-            w1_l, w2_l, w3_l, w4_l = len(w1_r), len(w2_r), len(w3_r), len(w4_r)
-            if len(w1_r) > 0 and sum(w1_r) > 0:
-                w1_r2 = np.mean(w1_r) * 7
-                w1_diff = np.abs(w1_p - w1_r2) / w1_r2
-            if len(w2_r) > 0 and sum(w2_r) > 0:
-                w2_r2 = np.mean(w2_r) * 7
-                w2_diff = np.abs(w2_p - w2_r2) / w2_r2
-            if len(w3_r) > 0 and sum(w3_r) > 0:
-                w3_r2 = np.mean(w3_r) * 7
-                w3_diff = np.abs(w3_p - w3_r2) / w3_r2
-            if len(w4_r) > 0 and sum(w4_r) > 0:
-                w4_r2 = np.mean(w4_r) * 7
-                w4_diff = np.abs(w4_p - w4_r2) / w4_r2
-            ret_list.append(
-                [id, w1_p, w2_p, w3_p, w4_p, w1_r2, w2_r2, w3_r2, r4_r2, w1_diff, w2_diff, w3_diff, w4_diff, w1_l, w2_l, w3_l, w4_l])
-            print(
-                f"id:{id}, w1_p:{w1_p}, w2_p:{w2_p}, w3_p:{w3_p}, w4_p:{w4_p},  w1_r2:{w1_r2}, w2_r2:{w2_r2}, w3_r2:{w3_r2}, r4_r2:{r4_r2}, "
-                f" w1_diff:{w1_diff}, w2_diff:{w2_diff}, w3_diff:{w3_diff}, w4_diff:{w4_diff},  w1_l:{w1_l}, w2_l:{w2_l}, w3_l:{w3_l}, w4_l:{w4_l} ")
-        ret_df = pd.DataFrame(ret_list,
-                              columns=['id', 'w1_p', 'w2_p', 'w3_p', 'w4_p', 'w1_r2', 'w2_r2', 'w3_r2', 'r4_r2',
-                                       'w1_diff', 'w2_diff', 'w3_diff', 'w4_diff', 'w1_l', 'w2_l', 'w3_l', 'w4_l'])
-        ret_df.to_csv(local_metrics_path)
+        metrics()
 
     print('all cost[hour]:', (time.time() - st1) / 3600)
 
