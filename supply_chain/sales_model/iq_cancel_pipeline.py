676e9439e5f1bd02478b862680435ac7c0ad6904
dehe.lai <dehe.lai@urbanic.com>
2025-05-16 14:45:00 +0800

    iq

--- supply_chain/sales_model/iq_cancel_pipeline.py
+++ supply_chain/sales_model/iq_cancel_pipeline.py
@@ -78,6 +78,11 @@ def main(args):
 
     if 'metrics' in args.pipeline:
         print('step5: 评测')
+        pred_df = parquet.read_table(s3_evaluated_result_path % (suffix, '20250514')).to_pandas()
+        real_df_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc_smooth_iq/ds=20250514/'
+        real_df =  parquet.read_table(real_df_file).to_pandas()
+        pred_df.head()
+        real_df.head()
 
 
     print('all cost[hour]:', (time.time() - st1) / 3600)
