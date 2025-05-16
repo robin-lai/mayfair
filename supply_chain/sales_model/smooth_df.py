4c3973adcc99e2eacb6f268a85f54d1830b99837
dehe.lai <dehe.lai@urbanic.com>
2025-05-16 14:06:26 +0800

    iq

--- supply_chain/sales_model/smooth_df.py
+++ supply_chain/sales_model/smooth_df.py
@@ -1,11 +1,24 @@
 from pyarrow import parquet
 import os
+import pandas as pd
 from common import smooth_flash
 
 
 def smooth_df(in_file, s3_file):
     df = parquet.read_table(in_file).to_pandas()
-    df2 = smooth_flash(df)
+    codes = set(df['skc_id'].values.tolist())
+    count = 0
+    df_list = []
+    for code in codes:
+        if count % 500 == 0:
+            print("step: %d / %d" % (count, len(codes)))
+        count += 1
+        sub_df = df[df['skc_id'] == code].copy(deep=True)
+        sub_df = sub_df.sort_values(by='target_date')
+        sub_df = smooth_flash(sub_df)
+        df_list.append(sub_df)
+
+    df2 = pd.concat(df_list)
     local_file_csv = './tmp/' + '_'.join(s3_file.split('/'))
     local_file_pt = './tmp/' + '_'.join(s3_file.split('/') + '.parquet')
     df2.to_csv(local_file_csv)
@@ -15,6 +28,12 @@ def smooth_df(in_file, s3_file):
 
 
 if __name__ == '__main__':
-    site_in_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc/ds=20250515/'
-    site_s3_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc_smooth/ds=20250515/'
+    # in
+    # site_in_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc/ds=20250515/'
+    # site_s3_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc_smooth/ds=20250515/'
+    # smooth_df(in_file=site_in_file, s3_file=site_s3_file)
+
+    # iq
+    site_in_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc_iq/ds=20250515/'
+    site_s3_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc_smooth_iq/ds=20250515/'
     smooth_df(in_file=site_in_file, s3_file=site_s3_file)
