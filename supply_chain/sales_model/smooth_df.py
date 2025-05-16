b22f3ea0de7de66b31d3fc794a99e2654b8f287a
dehe.lai <dehe.lai@urbanic.com>
2025-05-16 13:37:20 +0800

    iq

---
+++ supply_chain/sales_model/smooth_df.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from pyarrow import parquet
+
+in_file = '"s3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc/ds=20250515/'
+pt = parquet.read_table(in_file).to_pandas()
+print(pt.head(10))
