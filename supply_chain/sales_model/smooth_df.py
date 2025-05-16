be77d08906edb4d30e10d961e8a1a36e92c41517
dehe.lai <dehe.lai@urbanic.com>
2025-05-16 13:55:56 +0800

    iq

--- supply_chain/sales_model/smooth_df.py
+++ supply_chain/sales_model/smooth_df.py
@@ -1,6 +1,20 @@
-import pandas as pd
 from pyarrow import parquet
+import os
+from common import smooth_flash
 
-in_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc/ds=20250515/'
-pt = parquet.read_table(in_file).to_pandas()
-print(pt.head(10))
+
+def smooth_df(in_file, s3_file):
+    df = parquet.read_table(in_file).to_pandas()
+    df2 = smooth_flash(df)
+    local_file_csv = './tmp/' + '_'.join(s3_file.split('/'))
+    local_file_pt = './tmp/' + '_'.join(s3_file.split('/') + '.parquet')
+    df2.to_csv(local_file_csv)
+    df2.to_parquet(local_file_pt, engine="pyarrow")
+    os.system('aws cp %s %s' % (local_file_pt, s3_file))
+    print(df2.head(10))
+
+
+if __name__ == '__main__':
+    site_in_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc/ds=20250515/'
+    site_s3_file = 's3://warehouse-algo/sc_forecast_sequence_ts_model_train_and_predict_skc_smooth/ds=20250515/'
+    smooth_df(in_file=site_in_file, s3_file=site_s3_file)
