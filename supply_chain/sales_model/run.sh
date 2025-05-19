ef475c0ceab470549fa9dffd39ad4a2bdc8370fa
dehe.lai <dehe.lai@urbanic.com>
2025-05-19 15:17:12 +0800

    D

--- supply_chain/sales_model/run.sh
+++ supply_chain/sales_model/run.sh
@@ -23,5 +23,22 @@ elif [ $1 = "in_no_c" ]; then
     sleep 60
     python -u in_no_cancel_pipeline.py --pipeline=init,train,pred --time_delta=1
 elif [ $1 = "iq_c" ]; then
-    python iq_cancel_pipeline.py
+    python -u iq_cancel_pipeline.py --pipeline=init,train,pred --time_delta=18
+    sleep 60
+    python -u iq_cancel_pipeline.py --pipeline=init,train,pred --time_delta=17
+    sleep 60
+    python -u iq_cancel_pipeline.py --pipeline=init,train,pred --time_delta=16
+    sleep 60
+    python -u iq_cancel_pipeline.py --pipeline=init,train,pred --time_delta=15
+    sleep 60
+    python -u iq_cancel_pipeline.py --pipeline=init,train,pred --time_delta=14
+    sleep 60
+    python -u iq_cancel_pipeline.py --pipeline=init,train,pred --time_delta=13
+    sleep 60
+    python -u iq_cancel_pipeline.py --pipeline=init,train,pred --time_delta=12
+    sleep 60
+    python -u iq_cancel_pipeline.py --pipeline=init,train,pred --time_delta=11
+    sleep 60
+    python -u iq_cancel_pipeline.py --pipeline=init,train,pred --time_delta=10
+    sleep 60
 fi
