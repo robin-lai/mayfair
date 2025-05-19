b185bfa99b318eda2f6a7c5238e2c238e2a5d84a
dehe.lai <dehe.lai@urbanic.com>
2025-05-19 14:54:40 +0800

    D

--- supply_chain/sales_model/run.sh
+++ supply_chain/sales_model/run.sh
@@ -1,31 +1,31 @@
 if [ $1 = "in_c" ]; then
-    python -u in_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=7
+    python -u in_cancel_pipeline.py --pipeline=init,train,pred --time_delta=7
     sleep 60
-    python -u in_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=6
+    python -u in_cancel_pipeline.py --pipeline=init,train,pred --time_delta=6
     sleep 60
-    python -u in_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=5
+    python -u in_cancel_pipeline.py --pipeline=init,train,pred --time_delta=5
     sleep 60
-    python -u in_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=4
+    python -u in_cancel_pipeline.py --pipeline=init,train,pred --time_delta=4
     sleep 60
-    python -u in_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=3
+    python -u in_cancel_pipeline.py --pipeline=init,train,pred --time_delta=3
     sleep 60
-    python -u in_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=2
+    python -u in_cancel_pipeline.py --pipeline=init,train,pred --time_delta=2
     sleep 60
-    python -u in_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=1
+    python -u in_cancel_pipeline.py --pipeline=init,train,pred --time_delta=1
 elif [ $1 = "in_no_c" ]; then
-    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=7
+    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred --time_delta=7
     sleep 60
-    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=6
+    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred --time_delta=6
     sleep 60
-    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=5
+    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred --time_delta=5
     sleep 60
-    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=4
+    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred --time_delta=4
     sleep 60
-    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=3
+    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred --time_delta=3
     sleep 60
-    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=2
+    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred --time_delta=2
     sleep 60
-    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred,eval --time_delta=1
+    python -u in_no_cancel_pipeline.py --pipeline=init,train,pred --time_delta=1
 elif [ $1 = "iq_c" ]; then
     python gen_tfrecored_multi_process_with_gen_mt_sample_select.py --ds=$2
 fi
