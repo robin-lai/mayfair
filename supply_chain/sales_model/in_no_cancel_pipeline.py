8ec8db4c11b601c439fd9d2ae2eb9f32bff95225
dehe.lai <dehe.lai@urbanic.com>
2025-05-21 15:47:24 +0800

    D

--- supply_chain/sales_model/in_no_cancel_pipeline.py
+++ supply_chain/sales_model/in_no_cancel_pipeline.py
@@ -55,10 +55,10 @@ if __name__ == '__main__':
         epilog='sc-help')
     parser.add_argument('--pipeline', type=str,
                         default='init,train,pred')
-    parser.add_argument('--time_delta', type=int, default=7)
+    parser.add_argument('--time_delta', type=int, default=0)
     parser.add_argument('--pred_date_str', type=str, default="")
     parser.add_argument('--real_date_str', type=str, default="")
-    for  i in [2, 1]:
+    for  i in [0]:
         args = parser.parse_args()
         args.time_delta = i
         main(args)
