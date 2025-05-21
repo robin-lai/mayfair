b0874911b49cf472eff68f0a60fff62cf9f178b0
dehe.lai <dehe.lai@urbanic.com>
2025-05-21 16:17:18 +0800

    D

--- pkg_code.py
+++ pkg_code.py
@@ -2,3 +2,6 @@ import os
 os.system('git pull')
 os.system('find algo_rec -type f -name "*.py" | tar -czvf algo_rec.tar.gz -T -')
 os.system('aws s3 cp algo_rec.tar.gz s3://warehouse-algo/rec/code/algo_rec.tar.gz')
+
+os.system('find supply_chain -type f -name "*.py" | tar -czvf supply_chain.tar.gz -T -')
+os.system('aws s3 cp supply_chain.tar.gz s3://warehouse-algo/rec/code/supply_chain.tar.gz')
