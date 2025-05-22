174967f971bb8462c4c9bb3cc96ca50ef467b548
dehe.lai <dehe.lai@urbanic.com>
2025-05-22 15:28:39 +0800

    D

--- supply_chain/sales_model/common.py
+++ supply_chain/sales_model/common.py
@@ -578,8 +578,8 @@ def daily_predict_thread(proc_id, shared_list,saved_model, sequence_features, to
     count = 0
     except_n = 0
     n = len(to_predict_codes)
-    for code, to_predict_week_feature, sequence_feature in zip(to_predict_codes, to_predict_week_features,
-                                                               sequence_features)[0:1000]:
+    for code, to_predict_week_feature, sequence_feature in zip(to_predict_codes[0:1000], to_predict_week_features[0:1000],
+                                                               sequence_features[0:1000]):
         if count % 5000 == 0:
             print(f"proc_id:{proc_id} process count:{count} / {n}")
         week_num = to_predict_week_feature[1] + 1
