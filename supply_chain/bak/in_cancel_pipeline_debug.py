f9053d579ea8cf48d64bbd7798468d3279b0f30b
dehe.lai <dehe.lai@urbanic.com>
2025-05-23 11:25:50 +0800

    fix

---
+++ supply_chain/bak/in_cancel_pipeline_debug.py
@@ -0,0 +1,66 @@
+# import argparse
+# from pipeline import *
+#
+# base_dir = "./data_cancel_debug/"
+# data_path = "sc_forecast_sequence_ts_model_train_and_predict_skc/"
+# data_smooth_eval_path = base_dir + "sc_forecast_sequence_ts_model_train_and_predict_skc_smooth_eval.csv"
+# model_num = 1
+#
+# model_path = base_dir + "best_model.pth"
+# s3_model_path = 's3://warehouse-algo/sequence_model_predict_best_model_d/ds=%s/'
+# s3_pred_path = 's3://warehouse-algo/sequence_model_predict_result_d/ds=%s/'
+# s3_eval_path = 's3://warehouse-algo/sequence_model_evaluated_result_d/ds=%s/evaluated_result.parquet'
+#
+# local_data_path = base_dir + "sequence_data.csv"
+# local_metrics_path = base_dir + "metrics_skc_diff.csv"
+# tmp_path = base_dir + 'tmp.txt'
+# local_future_dau_plan_path = base_dir + "savana_future_daus.csv"
+# local_eval_path = base_dir + "evaluated_result.parquet"
+# local_pred_path = base_dir + 'output.parquet'
+# local_pred_dir = base_dir + 'pred/'
+#
+#
+# def main(args):
+#     st1 = time.time()
+#     os.system('rm -rf %s' % base_dir)
+#     os.system('mkdir %s' % base_dir)
+#     os.system('mkdir %s' % local_pred_dir)
+#     print('step1:process data')
+#     dc = DataConfig("sales_1d", args.time_delta)
+#
+#     # step 1: process data
+#     if 'init' in args.pipeline:
+#         init(dc, data_path, local_data_path, tmp_path)
+#         dc.init_df(local_data_path)
+#
+#     if 'train' in args.pipeline:
+#         train_pipeline(dc, model_path, s3_model_path)
+#
+#     if 'pred' in args.pipeline:
+#         pred(dc, model_num, s3_model_path, local_pred_dir, local_pred_path, s3_pred_path)
+#
+#     if 'eval' in args.pipeline:
+#         eval(dc, local_eval_path, local_pred_dir, data_smooth_eval_path, s3_eval_path)
+#
+#     if 'metrics' in args.pipeline:
+#         metrics(s3_pred_path, local_metrics_path, args.pred_date_str, args.real_date_str)
+#
+#     print('all cost[hour]:', (time.time() - st1) / 3600)
+#
+#
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(
+#         prog='sc_iq',
+#         description='sc_iq',
+#         epilog='sc-iq-help')
+#     parser.add_argument('--pipeline', type=str,
+#                         default='init,train,pred,eval,metrics')
+#     parser.add_argument('--time_delta', type=int, default=7)
+#     parser.add_argument('--pred_date_str', type=str, default="")
+#     parser.add_argument('--real_date_str', type=str, default="")
+#     args = parser.parse_args()
+#     for  i in [6, 5, 4, 3, 2, 1, 0]:
+#         args = parser.parse_args()
+#         args.time_delta = i
+#         main(args)
+#         alert_feishu(f"in_cancel_pipeline_debug process :{args.time_delta}")
